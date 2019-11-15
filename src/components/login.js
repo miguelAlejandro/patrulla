b3d0fb646ddc472d18cbb379897948543d6a1e94
miguelalejandro <alexpenarodriguez@gmail.com>
2019-11-15 04:59:24 -0400

    axios login in and login up

--- src/components/login.js
+++ src/components/login.js
@@ -1,22 +1,71 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/login.css';
-import { Form, Button, Row, Col, Container } from 'react-bootstrap';
-// import axios from 'axios';
+import { Form, Button, Row, Col, Container} from 'react-bootstrap';
+import axios from 'axios';
 
 function Login() {
 
-    // function LoginUp(e) {
-    //     // e.preventDefault();
-    //     console.log('Login Up');
-    //     console.log(e.target.elements.email.value);
-    //   }
+    function LoginUp(e) {
+        // e.preventDefault();
+        console.log('Login Up');
+        console.log(e.target.elements.email.value);
 
-    //   function LoginIp(e) {
-    //     // e.preventDefault();
-    //     console.log('Login In');
-    //     console.log(e.target.elements.email.value);
-    //   }
+        axios.post('/api/sign_up', {
+            
+            name: e.target.elements.user.value,
+            email: e.target.elements.email.value,
+            password: e.target.elements.password.value,
+            role: e.target.elements.type.value,
+            image: null,
+            
+          })
+          .then(function (response) {
+            console.log(response);
+            if(response){
+                alert("Login Up");
+            }
+          })
+          .catch(function (error) {
+            console.log(error);
+            if(error){
+                alert("Login Up Error: ${error}");
+            }
+          })
+          .then(function () {
+            // always executed
+          }); 
+
+      }
+
+      function LoginIn(e) {
+        // e.preventDefault();
+        console.log('Login In');
+        console.log(e.target.elements.email.value);
+        axios.post('/api/sign_in', {
+           
+            email: e.target.elements.email.value,
+            password: e.target.elements.password.value,
+            
+          })
+          .then(function (response) {
+            console.log(response);
+            if(response){
+                alert("Login In");
+                window.location.href = "/mapas";
+            }
+
+          })
+          .catch(function (error) {
+            console.log(error);
+            if(error){
+                alert("Login In Error: ${error}");
+            }
+          })
+          .then(function () {
+            // always executed
+          });
+      }
     return (
     <div className="body-login">    
         <Container>
@@ -30,7 +79,7 @@ function Login() {
 
                         <h2>Sign in</h2>
                         <hr></hr>
-                        <Form>
+                        <Form onSubmit={(e) => LoginIn(e)}>
                             <Form.Group controlId="email">
                                 <Form.Label>Email</Form.Label>
                                 <Form.Control type="text" placeholder="@email" />
@@ -43,7 +92,7 @@ function Login() {
                             </Form.Group>
                             <br></br>
                             <br></br>
-                            <Button variant="primary" type="submit" href="/mapas">
+                            <Button variant="primary" type="submit" >
                                 Login
                             </Button>
                         </Form>
@@ -58,7 +107,7 @@ function Login() {
                     <div className="Sign_up">
                         <h2>Sign up</h2>
                         <hr></hr>
-                        <Form>
+                        <Form  onSubmit={(e) => LoginUp(e)}>
                             <Form.Group controlId="user">
                                 <Form.Label>User</Form.Label>
                                 <Form.Control type="text" placeholder="user" />
