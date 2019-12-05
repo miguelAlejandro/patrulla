65e3dd80b66b1e17f5f3d70d98b771dd3bce6825
Jorgevargasjvm <jorgevargasmateo@gmail.com>
2019-12-05 02:52:45 -0400

    add patrullas sensores alertas de la API

--- src/components/mapas.js
+++ src/components/mapas.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import './css/mapas.css';
-
+import axios from 'axios';
+import useAxios from 'axios-hooks'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Row, Col, Button, Container } from 'react-bootstrap';
 import SimpleMap from './google_map';
@@ -23,30 +24,41 @@ function enviar(id) {
 }
 function MapasAuthOff() {
   return (<div>
-      <div className="body-manager">
-          <Container>
-              <Row>
-                  <Col xs lg={12}>
-                  <h1>Necesita iniciar seccion</h1>
-                  </Col>
-              </Row>
-          </Container>
+    <div className="body-manager">
+      <Container>
+        <Row>
+          <Col xs lg={12}>
+            <h1>Necesita iniciar seccion</h1>
+          </Col>
+        </Row>
+      </Container>
 
-      </div>
-      
+    </div>
+
   </div>);
 }
-function MapasAuthOn({state}) {
-  const alertas = data.docs[0].alertas;
-  const sensores = data.docs[0].sensores;
-  // const patrullas = data.docs[0].patrullas;
+function MapasAuthOn({ state }) {
+  // const alertas = data.docs[0].alertas;
+  // const sensores = data.docs[0].sensores;
+  const [{ alertas, loading_alerta, error_alerta }, refetch] = useAxios(
+    'http://localhost:3000/api/read_alertas'
+  )
+  const [{ sensores, loading_sensores, error_sensores }, refetch] = useAxios(
+    'http://localhost:3000/api/read_sensores'
+  )
+  const [{ patrullas, loading_patrullas, error_patrullas }, refetch] = useAxios(
+    'http://localhost:3000/api/read_patrullas'
+  )
+  if (loading_alerta && loading_sensores && loading_patrullas) return <p>Loading...</p>
+  if (error_alerta && error_sensores && error_patrullas) return <p>Error!</p>
+
   const notificaciones = alertas.map((alerta, id) =>
     <div key={id + "-nt"} id={id + "-nt"} >
       <br></br>
       <div className="nota">
         <Row>
           <Col xs lg={6}>
-            <h4>{alerta.name}</h4>
+            <h4>{alerta.nombre}</h4>
           </Col>
           <Col xs lg={6}>
             <div className="button-enviar-cerrar">
@@ -60,13 +72,12 @@ function MapasAuthOn({state}) {
         <Row>
           <Col xs lg={6}>
             <hr></hr>
-            <h6>Valor: {alerta.val}</h6>
-            <h6>Valor Maximo: {alerta.valMaximo}</h6>
-            <h6>Valor Minimo: {alerta.ValMinimo}</h6>
+            <h6>Email: {alerta.emailDestino}</h6>
+            <h6>Ubicacion: {alerta.ubicacion}</h6>
+            <h6>Serial: {alerta.serial}</h6>
           </Col>
           <Col xs lg={6}>
             <hr></hr>
-            <h6>Ubicacion: {alerta.ubicacion}</h6>
             <h6>Hora: {alerta.hora}</h6>
             <h6>Fecha: {alerta.fecha}</h6>
             <hr></hr>
@@ -74,8 +85,8 @@ function MapasAuthOn({state}) {
         </Row>
         <Row>
           <Col xs lg={12}>
-            <h6>Detalles:</h6>
-            <p>Lorem ipsum dolor sit amet consectetur adipiscing, elit duis tempus fringilla id. Maecenas dictum hendrerit tortor placerat</p>
+            <h6>Informacion:</h6>
+            <p>{alerta.informacion}</p>
           </Col>
         </Row>
       </div>
@@ -86,7 +97,7 @@ function MapasAuthOn({state}) {
       <div key={id + "-ls"} className="listaDesensores">
         <Row>
           <Col xs lg={12}>
-            <h4>{sensor.name}</h4>
+            <h4>{sensor.nombre}</h4>
           </Col>
         </Row>
         <Row>
@@ -100,7 +111,24 @@ function MapasAuthOn({state}) {
             <h6>Ubicacion : {sensor.ubicacion}</h6>
           </Col>
           <Col xs lg={12}>
-            <h6>Valor = {sensor.val}</h6>
+            <h6>Email : {sensor.email}</h6>
+          </Col>
+        </Row>
+        <hr></hr>
+      </div>
+    </Col>
+  );
+  const listaDePatrullas = patrullas.map((patrulla, id) =>
+    <Col xs lg={4}>
+      <div key={id + "-ps"} className="listaDesensores">
+        <Row>
+          <Col xs lg={12}>
+            <h4>{patrulla.nombre}</h4>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs lg={12}>
+            <h6>Email = {patrulla.email}</h6>
           </Col>
         </Row>
         <hr></hr>
@@ -155,7 +183,7 @@ function MapasAuthOn({state}) {
                   <h4>Lista de patrullas</h4>
                   <Row className="sensore_bar">
                     {
-                      listaDeSensores
+                      listaDePatrullas
                     }
                   </Row>
                 </div>
@@ -167,18 +195,18 @@ function MapasAuthOn({state}) {
     </div>
   );
 }
-function Mapas({state}){
-  if(state.email && state.token){
+function Mapas({ state }) {
+  if (state.email && state.token) {
     return < MapasAuthOn />
   }
   return < MapasAuthOff />
 }
 
 const mapStateToProps = state => ({
-    state: state
+  state: state
 })
 const mapDispatchToProps = dispatch => ({
-  
+
 
 });
 
