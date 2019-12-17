10dbcfc8039b9e004cde8b61195061c0bfc4c351
Jorgevargasjvm <jorgevargasmateo@gmail.com>
2019-12-17 17:58:31 -0400

    sensores y patrulla lista en el map

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -1,84 +1,68 @@
 import React from 'react';
+import useAxios from 'axios-hooks';
 import GoogleMapReact from 'google-map-react';
 import "./css/sensor.css";
+import "./css/patrulla.css";
 
-const Sensor = ({ text }) => < div className="sensor" > {text} < /div>;
-const Patrulla = ({text}) => < div className="patrulla" > {text} < /div>;
-    
+function Sensor({ text }) {
+
+    return (<div className="sensor">{text}</div>
+    );
+}
+
+function Patrulla({ text }) {
+    return (<div className="patrulla">{text}</div>);
+}
+
 function SimpleMap(props) {
-
+    const [{ data: getSensores, loading: getSensoresLoading, error: getSensoresError }] = useAxios(
+        'http://localhost:3000/api/read_sensores'
+    )
+    const [{ data: getPatrullas, loading: getPatrullasLoading, error: getPatrullasError }] = useAxios(
+        'http://localhost:3000/api/read_patrullas'
+    )
+    if (getSensoresLoading || getPatrullasLoading) return <p>Loading...</p>
+    if (getSensoresError || getPatrullasError) return <p>Error!</p>
 
     const defaultProps = {
-            center: {
+        center: {
             lat: 19.4399935,
-        lng: -70.7430644
-    },
-    zoom: 12
-};
+            lng: -70.7430644
+        },
+        zoom: 12
+    };
+
+    console.log(getSensores)
+    console.log(getPatrullas)
 
     const handleApiLoaded = (map, maps) => {
-            // use map and maps objects
-        };
-        return (
-            // Important! Always set the container height explicitly
-    
-        <div style={
-                { height: '95vh', padding: '20px' }} >
-            <GoogleMapReact bootstrapURLKeys={
-                    { key: "AIzaSyDOwoAFQVTjgqYs1r5f7j_6Y4yr5er2-v8" }}
+        // use map and maps objects
+    };
+    return (
+        // Important! Always set the container height explicitly
+
+        <div style={{ height: '95vh', padding: '20px' }} >
+
+            <GoogleMapReact bootstrapURLKeys={{ key: "AIzaSyDOwoAFQVTjgqYs1r5f7j_6Y4yr5er2-v8" }}
                 defaultCenter={defaultProps.center}
                 defaultZoom={defaultProps.zoom}
-                yesIWantToUseGoogleMapApiInternals onGoogleApiLoaded={
-                    ({ map, maps }) => handleApiLoaded(map, maps)} >
+                yesIWantToUseGoogleMapApiInternals onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)} >
 
-                <Sensor lat={19.4399935}
-                    lng={-70.7430644}
-                    text="Sensor 1" /
-                >
-                <Sensor lat={19.4299935}
-                    lng={-70.2430644}
-                    text="Sensor 2" /
-                >
-                <Sensor lat={19.1399935}
-                    lng={-70.1430644}
-                    text="Sensor 3" /
-                >
-                <Sensor lat={19.4399935}
-                    lng={-70.9430644}
-                    text="Sensor 4" /
-                >
-                <Sensor lat={19.5399935}
-                    lng={-70.3430644}
-                    text="Sensor 5" /
-                >
-                <Patrulla lat={19.1299935}
-                    lng={-70.6430644}
-                    text="patrulla 1" /
-                >
-                <Patrulla lat={19.6399935}
-                    lng={-70.3430644}
-                    text="patrulla 2" /
-                >
-                <Patrulla lat={19.3399935}
-                    lng={-70.6430644}
-                    text="patrulla 3" /
-                >
-                <Patrulla lat={19.2399935}
-                    lng={-70.5430644}
-                    text="patrulla 4" /
-                >
-                <Patrulla lat={19.6399935}
-                    lng={-70.7430644}
-                    text="patrulla 5" /
-                >
-                <Patrulla lat={19.5399935}
-                    lng={-70.1430644}
-                    text="patrulla 6" /
-                >
-                </GoogleMapReact> </div>
-        
-        
-            );
-        
-        }
+                {getSensores.sensores.map((d) =>  <Sensor lat={parseFloat(d.ejeX)}
+                    lng={parseFloat(d.ejeY)}
+                    text={d.nombre} /
+                >)}
+               {getPatrullas.patrullas.map((p) =>  <Patrulla lat={parseFloat(p.ejeX)}
+                    lng={parseFloat(p.ejeY)}
+                    text={p.nombre} /
+                >)}
+
+
+            </GoogleMapReact>
+        </div>
+
+
+    );
+
+}
 export default SimpleMap;
